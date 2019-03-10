0cec1e7c98d4527a2ca5546b59058113c25b35e5
Hongyi Zhang <hongyizhang@Hongyis-MacBook-Pro.local>
2019-03-09 19:19:54 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Color from 'color';
+const GREY = '#E8E8E8'
+
 
 class App extends Component {
+  state = {
+    color: '#E8E8E8'
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className='panel-container' 
+          onMouseLeave={() => this.setState({ color: GREY })}>
+          {
+            [
+              { id: 1, section: 'about', color: '#69cef4' },
+              { id: 2, section: 'profile', color: '#92cea5' },
+              { id: 3, section: 'title', color: '#e50b0b' },
+              { id: 4, section: 'life', color: '#e21493' },
+              { id: 5, section: 'experience', color: '#bfe214' },
+              { id: 6, section: 'work', color: '#f93abf' },
+              { id: 7, section: 'hobbies', color: '#11d684' },
+            ].map(item => {
+              const clr = Color(this.state.color)
+                .darken(.1 * item.id)
+                .saturate(0.5) 
+              return <div 
+                style={{
+                  color: clr.negate().blacken(1).hex(),
+                  backgroundColor: clr.hex()
+                }}
+                key={item.id} 
+                onMouseEnter={() => this.setState({ color: item.color })}
+                className='panel'
+              >{item.section}</div>
+            })
+          }
+        </div>
       </div>
     );
   }
